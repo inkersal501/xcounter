6cac46ce267169baf24c6f06fb4f2a792489c177
inkersal501 <inkersal501@gmail.com>
2024-10-02 21:23:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+import './App.css';
+
+function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleIncrement = ()=>{
+    setNumber((number)=>number+3);
+  };
+  const handleDecrement = ()=>{
+    setNumber((number)=>number-2);
+  };
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count: {number}</p>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
